8fead0f5be84f6a6e791105138009d46d5629891
Constantin Nickel <constantin.nickel@gmail.com>
2019-06-09 10:26:47 +0200

    webhook example for warp

---
+++ examples/webhook/src/bin/send.rs
@@ -0,0 +1,25 @@
+use dbl::model::{Webhook, WebhookType};
+use reqwest::header::AUTHORIZATION;
+
+fn main() {
+    let url = "http://localhost:3030/dbl/webhook";
+    let secret = "mywebhook";
+
+    let data = Webhook {
+        bot: 1234.into(),
+        user: 2345.into(),
+        kind: WebhookType::Test,
+        is_weekend: false,
+        query: None,
+    };
+
+    if let Err(e) = reqwest::Client::new()
+        .post(url)
+        .header(AUTHORIZATION, secret)
+        .json(&data)
+        .send()
+        .and_then(|resp| resp.error_for_status())
+    {
+        eprintln!("{}", e);
+    }
+}
---
+++ examples/webhook/src/bin/warp.rs
@@ -0,0 +1,57 @@
+use dbl::model::Webhook;
+use warp::body::BodyDeserializeError;
+use warp::http::StatusCode;
+use warp::path;
+use warp::{Filter, Rejection, Reply};
+
+fn main() {
+    let secret = "mywebhook";
+
+    let filter = warp::header::<String>("authorization")
+        .and_then(move |value| {
+            if value == secret {
+                Ok(())
+            } else {
+                Err(warp::reject::custom(Unauthorized))
+            }
+        })
+        .untuple_one();
+    let webhook = warp::post2()
+        .and(path!("dbl" / "webhook"))
+        .and(filter)
+        .and(warp::body::json())
+        .map(|hook: Webhook| {
+            println!("{:?}", hook);
+            warp::reply()
+        })
+        .recover(custom_error);
+
+    warp::serve(webhook).run(([127, 0, 0, 1], 3030));
+}
+
+fn custom_error(err: Rejection) -> Result<impl Reply, Rejection> {
+    if err.find_cause::<BodyDeserializeError>().is_some() {
+        Ok(warp::reply::with_status(
+            warp::reply(),
+            StatusCode::BAD_REQUEST,
+        ))
+    } else if err.find_cause::<Unauthorized>().is_some() {
+        Ok(warp::reply::with_status(
+            warp::reply(),
+            StatusCode::UNAUTHORIZED,
+        ))
+    } else {
+        Err(err)
+    }
+}
+
+#[derive(Debug)]
+struct Unauthorized;
+
+impl std::fmt::Display for Unauthorized {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("Unauthorized")
+    }
+}
+
+impl std::error::Error for Unauthorized {}
