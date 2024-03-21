aea8b22a203645ccea67891b927a96c13ded00c9
Constantin Nickel <constantin.nickel@gmail.com>
2024-03-21 17:05:26 +0100

    Bump version

--- src/lib.rs
+++ src/lib.rs
@@ -36,7 +36,7 @@
 //!     }
 //! }
 //! ```
-#![doc(html_root_url = "https://docs.rs/dbl-rs/0.3.2")]
+#![doc(html_root_url = "https://docs.rs/dbl-rs/0.4.0")]
 #![deny(rust_2018_idioms)]
 
 use reqwest::header::AUTHORIZATION;
